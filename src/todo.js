37443de830552f72c186bb379c1ec5d33a87e8b3
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2021-06-03 15:44:28 +0900

    adjust to async function

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,19 @@
 
-const showMessage = () => {
+// const showMessage = () => {
+//     const textbox = document.getElementById("input-message");
+//     const inputValue = textbox.value;
+//     const outputText = document.getElementById("output-message")
+//     if (inputValue === "") {
+//         outputText.className = "flex justify-center p-4 mx-auto mt-4 bg-red-300"
+//         outputText.innerText = 'タスクを記入してください'
+//     }
+//     else {
+//         outputText.className = "flex justify-center p-4 mx-auto mt-4 bg-gray-50 rounded-tl-xl rounded-br-xl"
+//         outputText.innerText = inputValue + 'を追加しました'
+//     }
+// }
+
+async function asyncShowMessage() {
     const textbox = document.getElementById("input-message");
     const inputValue = textbox.value;
     const outputText = document.getElementById("output-message")
@@ -12,4 +26,4 @@ const showMessage = () => {
         outputText.innerText = inputValue + 'を追加しました'
     }
 }
-
+asyncShowMessage()
