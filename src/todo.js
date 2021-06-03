16559bef3ac0fe8297203ea358fa7afe0a4a5c05
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2021-06-03 15:33:16 +0900

    finished giving value to one place

---
+++ src/todo.js
@@ -0,0 +1,15 @@
+
+const showMessage = () => {
+    const textbox = document.getElementById("input-message");
+    const inputValue = textbox.value;
+    const outputText = document.getElementById("output-message")
+    if (inputValue === "") {
+        outputText.className = "flex justify-center p-4 mx-auto mt-4 bg-red-300"
+        outputText.innerText = 'タスクを記入してください'
+    }
+    else {
+        outputText.className = "flex justify-center p-4 mx-auto mt-4 bg-gray-50 rounded-tl-xl rounded-br-xl"
+        outputText.innerText = inputValue + 'を追加しました'
+    }
+}
+
