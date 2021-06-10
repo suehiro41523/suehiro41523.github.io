93ec055b60110becb646988ae4265e85fe953984
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2021-06-10 13:51:46 +0900

    adjusted commiting file

--- src/todo.js
+++ src/todo.js
@@ -1,29 +1,34 @@
+"use strict";
 
-// const showMessage = () => {
-//     const textbox = document.getElementById("input-message");
-//     const inputValue = textbox.value;
-//     const outputText = document.getElementById("output-message")
-//     if (inputValue === "") {
-//         outputText.className = "flex justify-center p-4 mx-auto mt-4 bg-red-300"
-//         outputText.innerText = 'タスクを記入してください'
-//     }
-//     else {
-//         outputText.className = "flex justify-center p-4 mx-auto mt-4 bg-gray-50 rounded-tl-xl rounded-br-xl"
-//         outputText.innerText = inputValue + 'を追加しました'
-//     }
-// }
 
-async function asyncShowMessage() {
-    const textbox = document.getElementById("input-message");
-    const inputValue = textbox.value;
-    const outputText = document.getElementById("output-message")
-    if (inputValue === "") {
-        outputText.className = "flex justify-center p-4 mx-auto mt-4 bg-red-300"
-        outputText.innerText = 'タスクを記入してください'
+function ShowMessage() {
+    // 変数宣言
+    const textbox = document.getElementById('textbox');
+    const textValue = textbox.value;
+    const showInput = document.getElementById('showInput');
+    const showList = document.getElementById('showList');
+    const createList = document.createElement('li');
+    const length = document.getElementById('length');
+    const showLength = showList.childElementCount + 1
+    const footer = document.getElementsByTagName('footer')
+    // 条件分岐
+    if (textValue == "") {
+        showInput.className = 'flex p-4 mx-auto mt-2 bg-red-200 rounded-lg'
+        showInput.innerText = 'タスクを入力してください'
     }
     else {
-        outputText.className = "flex justify-center p-4 mx-auto mt-4 bg-gray-50 rounded-tl-xl rounded-br-xl"
-        outputText.innerText = inputValue + 'を追加しました'
+        showInput.className = 'flex justify-center px-4 mx-8 mt-2 text-left bg-gray-200 rounded-lg'
+        showInput.innerText = `${textValue}\nが追加されました`;
+        showList.appendChild(createList).innerText = textValue
+        length.className = 'flex justify-center py-2 mx-auto text-center bg-blue-200'
+        length.innerText = `${showLength} 個タスクがあります`;
+        function clear() {
+            const textValue = document.getElementById('textbox');
+            textValue.value = '';
+        }
+        clear()
     }
+    return false;
 }
-asyncShowMessage()
+
+ShowMessage();
