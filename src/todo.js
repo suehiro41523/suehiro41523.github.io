e7f30123770f0032cb38f0f56b77e83f00bdca60
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2021-06-17 17:37:57 +0900

    add utility class name for list

--- src/todo.js
+++ src/todo.js
@@ -8,6 +8,7 @@ function ShowMessage() {
     const showInput = document.getElementById('showInput');
     const showList = document.getElementById('showList');
     const createList = document.createElement('li');
+    const List = document.getElementsByTagName('li')
     const length = document.getElementById('length');
     const showLength = showList.childElementCount + 1
     const footer = document.getElementsByTagName('footer')
@@ -17,13 +18,14 @@ function ShowMessage() {
         showInput.innerText = 'タスクを入力してください'
     }
     else {
-        showInput.className = 'flex justify-center px-4 mx-8 mt-2 text-left bg-gray-200 rounded-lg'
-        showInput.innerText = `${textValue}\nが追加されました`;
+        showInput.className = 'flex justify-center px-4 mx-8 mt-2 text-left bg-gray-200 rounded-lg '
+        showInput.innerText = `${textValue}\nが追加されました`
         showList.appendChild(createList).innerText = textValue
+        List.className = 'w-max'
         length.className = 'flex justify-center py-2 mx-auto text-center bg-blue-200'
         length.innerText = `${showLength} 個タスクがあります`;
         function clear() {
-            const textValue = document.getElementById('textbox');
+            const textValue = document.getElementById('textbox')
             textValue.value = '';
         }
         clear()
