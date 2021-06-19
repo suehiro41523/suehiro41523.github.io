4a8f1e583655a1f6ce44ff74ccac5c7fe3ff2faa
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2021-06-19 18:18:07 +0900

    adjust tailwind file constructor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@ module.exports = {
   purge: [
     './src/**/*.html',
     './src/**/*.js',
-  ], darkMode: false, // or 'media' or 'class'
+    './index.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    container: {
-      center: true,
-    },
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 }
