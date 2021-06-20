bccc62a69a8c679fbbfae6cfb89082c8fb9a4e42
suehiro41523 <67720039+suehiro41523@users.noreply.github.com>
2021-06-20 11:45:58 +0900

    add background to help

--- src/discription.js
+++ src/discription.js
@@ -2,9 +2,24 @@
 
 const help = document.getElementById('help').getElementsByClassName('help')[0]
 const discription = document.getElementById('discription');
+const background = document.getElementsByClassName('background')[0]
 
 help.addEventListener('click', function () {
-    discription.classList.toggle('hidden')
+    discription.classList.toggle('hidden');
+    background.classList.toggle('hidden');
 })
 
-console.log(help);
+
+document.addEventListener('click', (e) => {
+    console.log(e.path);
+    if (e.path.includes(discription) || e.path.includes(help)) {
+        console.log(discription.className.includes('hidden'));
+    }
+    else {
+        discription.classList.add('hidden')
+        background.classList.add('hidden')
+    }
+})
+
+
+
--- src/todo.js
+++ src/todo.js
@@ -32,5 +32,5 @@ function ShowMessage() {
     }
     return false;
 }
-
 ShowMessage();
+
